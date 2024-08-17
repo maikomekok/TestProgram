ba541c595ff2c6d50af26a98560b0a76ce3b4351
maikomekok <82836785+maikomekok@users.noreply.github.com>
2024-08-17 20:34:17 +0400

    commented function

--- flask-app.py
+++ flask-app.py
@@ -9,11 +9,12 @@ df = pd.read_csv("2024-07-14_00_00_06_186018_BTC_rawdata_1.csv")
 
 @app.route('/')
 
-def index():
-    return render_template("index.html")
+# def index():
+#     return render_template("index.html")
 def data():
     fig = px.line(df,x = "date",y = "prc_vol1",title="persent of volume over time")
     graphJSON = fig.to_json()
     return  jsonify(graphJSON)
+
 if __name__=='__main__':
     app.run(debug=True)
